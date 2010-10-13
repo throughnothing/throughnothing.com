07437355c683b8f964575295a1f3ff91a3f50a66
William Wolf <throughnothing@gmail.com>
2010-10-13 20:29:46 +0000

    added lightbox to homepage images

--- js/photos.js
+++ js/photos.js
@@ -1,6 +1,10 @@
-function get_flickr_photo_url(p){
+function get_flickr_photo_url(p,size){
+	if(!size)
+		size = "";
+	else
+		size = "_" + size;
 	return "http://farm" + p.farm + ".static.flickr.com/" + 
-		p.server + "/" + p.id + "_" + p.secret + "_s.jpg";
+		p.server + "/" + p.id + "_" + p.secret + size + ".jpg";
 }
 function get_flickr_page(p){
 	return "http://www.flickr.com/photos/" + FLICKR_USER_ID + '/' + p.id + '/';
@@ -21,8 +25,8 @@ function random_photos(){
 function random_photo(img, photos){
 	var r = Math.floor(Math.random()*photos.length);
 	var p = photos[r];
-	img.attr("src", get_flickr_photo_url(p));
-	img.parent().attr("href", get_flickr_page(p));
+	img.attr("src", get_flickr_photo_url(p,'s'));
+	img.parent().attr("href", get_flickr_photo_url(p,'z'));
 	img.parent().attr("target", "_new");
 	img.parent().attr("alt", p.title);
 	photos.splice(r,1);
