e5af0c0e23dbd493a10e97c48dc60b5dd58b8b0f
William Wolf <throughnothing@gmail.com>
2010-10-13 23:30:17 +0000

    add js for quotes

---
+++ _js/quotes.js
@@ -0,0 +1,8 @@
+var quotes = [{"quote":"A good traveler has no fixed plans, and is not intent on arriving","author":"Lao Tzu"},
+{"quote":"The world is a book and those who do not travel read only one page", "author": "St. Augustine"},
+{"quote":"One's destination is never a place, but a new way of seeing things.", "author": "Henry Miller"},
+{"quote":"A journey of a thousand miles must begin with a single step.", "author": "Lao Tzu"},
+{"quote":"A journey is best measured in friends, rather than miles.", "author": "Tim Cahill"},
+{"quote":"Not all those who wander are lost.", "author": "J. R. R. Tolkein"},
+{"quote":"It is better to travel than to arrive.", "author": "Buddha"}
+]
