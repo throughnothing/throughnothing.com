78728f94629fc544ce08a8feba3172fd870135e5
William <throughnothing@gmail.com>
2010-10-13 20:15:30 -0400

    update quotes randomization for site

--- _js/quotes.js
+++ _js/quotes.js
@@ -4,5 +4,23 @@ var quotes = [{"quote":"A good traveler has no fixed plans, and is not intent on
 {"quote":"A journey of a thousand miles must begin with a single step.", "author": "Lao Tzu"},
 {"quote":"A journey is best measured in friends, rather than miles.", "author": "Tim Cahill"},
 {"quote":"Not all those who wander are lost.", "author": "J. R. R. Tolkein"},
-{"quote":"It is better to travel than to arrive.", "author": "Buddha"}
+{"quote":"It is better to travel than to arrive.", "author": "Buddha"},
+{"quote":"To travel is to discover that everyone is wrong about other countries.", "author": "Aldous Huxley"},
+{"quote":"Travel and change of place impart new vigor to the mind.", "author": "Seneca"}
 ]
+
+var cur_quote;
+function change_quote(){
+	var r = Math.floor(Math.random()*quotes.length);
+	while(r == cur_quote)
+		var r = Math.floor(Math.random()*quotes.length);
+
+	var q = $('span.quote');
+	q.html('"' + quotes[r].quote + '" <span> - ' + quotes[r].author + '</span>');
+	q.fadeIn(2000);
+}
+
+$(document).ready(function(){
+		change_quote();
+		setInterval(function(){ $('span.quote').fadeOut(2000,change_quote); },40000);
+});
