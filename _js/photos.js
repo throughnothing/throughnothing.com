0eb32b68a4a617dcee4b475b11c3de53b476b09f
William <throughnothing@gmail.com>
2010-10-13 20:49:15 -0400

    made smoother image transitions on home page thumbnails

--- _js/photos.js
+++ _js/photos.js
@@ -9,7 +9,7 @@ function get_flickr_photo_url(p,size){
 function get_flickr_page(p){
 	return "http://www.flickr.com/photos/" + FLICKR_USER_ID + '/' + p.id + '/';
 }
-function random_photos(){
+function random_photos(duration){
 	var photos = [];
 	var sets_in_latest_collection = FLICKR_TRAVEL_COLLECTION.collection[0].set.length;
 	// Concatenate the latest Collection sets into one
@@ -19,19 +19,24 @@ function random_photos(){
 	//copy array
 	photos = photos.slice(0,photos.length);
 	$.each($('#hp_images img'),function(i,img){
-		random_photo($(img), photos);
+		img = $(img);
+		random_photo($(img), photos,i,duration);
 	});
 }
-function random_photo(img, photos){
-	var r = Math.floor(Math.random()*photos.length);
-	var p = photos[r];
-	img.attr("src", get_flickr_photo_url(p,'s'));
-	img.parent().attr("href", get_flickr_photo_url(p,'z'));
-	img.parent().attr("target", "_new");
-	img.parent().attr("alt", p.title);
-	photos.splice(r,1);
+function random_photo(img, photos,i,duration){
+	img.delay(duration*i).fadeOut('slow',function(){
+		var r = Math.floor(Math.random()*photos.length);
+		var p = photos[r];
+		photos.splice(r,1);
+		img.attr("src", get_flickr_photo_url(p,'s'));
+		img.parent().attr("href", get_flickr_photo_url(p,'z'));
+		img.parent().attr("target", "_new");
+		img.parent().attr("alt", p.title);
+		img.delay(400).fadeIn('slow');
+	});
+
 }
 $(document).ready(function(){
-	random_photos();
-	setInterval("random_photos()",10000);
+	random_photos(0);
+	setInterval('random_photos(1000)',20000);
 });
